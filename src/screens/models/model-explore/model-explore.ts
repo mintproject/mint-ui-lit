45dbe90814f54a577f189570149dc56b5546e1a6
Hernán Vargas <hern.var@gmail.com>
2019-08-06 13:11:59 -0400

    fixed search models

--- src/screens/models/model-explore/model-explore.ts
+++ src/screens/models/model-explore/model-explore.ts
@@ -149,7 +149,7 @@ export class ModelExplorer extends connect(store)(PageViewElement) {
                         return html`
                         <model-facet 
                             uri="${key}"
-                            style .= "${st}">
+                            style="${st}">
                         </model-facet>
                         `
                     })}
