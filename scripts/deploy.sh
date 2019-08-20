2e5831ed7c3bd6bca89725c5be36655350856dcf
Maximiliano Osorio <maxiosorio@gmail.com>
2019-08-20 10:41:06 -0400

    Fix: rsync script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -xe
 SOURCE_DIR=$1
 DEST_DIR=$2
 
@@ -8,5 +9,5 @@ if [ "$#" -ne 2 ]; then
 fi
 echo "Tag: $TRAVIS_TAG"
 echo "Branch:  $TRAVIS_BRANCH"
-rsync -r --delete-after --quiet $SOURCE_DIR  mintui@mint.isi.edu:$DEST_DIR
+rsync -r --delete-after $SOURCE_DIR  mintui@mint.isi.edu:$DEST_DIR
 exit $?	
