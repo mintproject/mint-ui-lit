79fe2b41d0f262c375f892ad1649041bcd9cd026
Hernán Vargas <hern.var@gmail.com>
2019-08-23 11:36:16 -0400

    added gallery to regions/hydrology and regions/agriculture

--- src/screens/regions/regions-agriculture.ts
+++ src/screens/regions/regions-agriculture.ts
@@ -6,6 +6,8 @@ import { SharedStyles } from '../../styles/shared-styles';
 import { store } from '../../app/store';
 import { connect } from 'pwa-helpers/connect-mixin';
 
+import '../../components/image-gallery'
+
 @customElement('regions-agriculture')
 export class RegionsAgriculture extends connect(store)(PageViewElement) {
 
@@ -15,21 +17,38 @@ export class RegionsAgriculture extends connect(store)(PageViewElement) {
             .cltrow wl-button {
                 padding: 2px;
             }
+            @media (min-width: 1025px) { 
+                .content {
+                    width: 75%;
+                }
+            }
+            @media (max-width: 1024) { 
+                .content {
+                    width: 100%;
+                }
+            }
+            .content {
+                margin: 0 auto;
+            }
             `,
             SharedStyles
         ];
     }
 
     protected render() {
+        let items = [{
+            label: "Land use map of the Pongo Basin in South Sudan",
+            src: "https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/PIHMtoAgri/map_Landuse.png"}]
         return html`
-        <p>
-        This page is in progress, it will allow you to run tools to identify agricultural regions of interest.
-        Below are some example agricultural regions identified for South Sudan:
-        <ul>
-        <li><a href="https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/PIHMtoAgri/map_Landuse.png">Land Use Map</a
-            >. Land use map of the Pongo Basin in South Sudan</li>
-        </ul>        
-        </p>
+        <div class="content">
+            <p>
+                This page is in progress, it will allow you to run tools to identify agricultural regions of interest.
+                Below are some example agricultural regions identified for South Sudan:
+            </p>
+            <div style="width: 90%; margin: 0px auto;">
+                <image-gallery style="--width: 300px; --height: 160px;" .items="${items}"></image-gallery>
+            </div>
+        </div>
         `
     }
 }
--- src/screens/regions/regions-hydrology.ts
+++ src/screens/regions/regions-hydrology.ts
@@ -6,6 +6,8 @@ import { SharedStyles } from '../../styles/shared-styles';
 import { store } from '../../app/store';
 import { connect } from 'pwa-helpers/connect-mixin';
 
+import '../../components/image-gallery'
+
 @customElement('regions-hydrology')
 export class RegionsHydrology extends connect(store)(PageViewElement) {
 
@@ -15,37 +17,57 @@ export class RegionsHydrology extends connect(store)(PageViewElement) {
             .cltrow wl-button {
                 padding: 2px;
             }
+            @media (min-width: 1025px) { 
+                .content {
+                    width: 75%;
+                }
+            }
+            @media (max-width: 1024) { 
+                .content {
+                    width: 100%;
+                }
+            }
+            .content {
+                margin: 0 auto;
+            }
             `,
             SharedStyles
         ];
     }
 
     protected render() {
+        let items = [
+            {   label: "South Sudan River Basins (PIHM)",
+                src: "https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/PIHMRiverBasin/2017.png",
+                desc: "The three river basins that were our focus in 2018."},
+            {   label: "South Sudan River Basins - POI",
+                src: "https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/PIHMRiverBasin/POI.png",
+                desc: "The three river basins that were our focus in 2018 (overlayed with points of interest)."},
+            {   label: "Ethiopia relief boundary",
+                src: "https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/ImproveQuality/Ethiopia_relief_boundary.png"},
+            /*{   label: "Ethiopia relief subbasins (94MB)",
+                src: "http://mint.isi.edu/dev/data/Ethiopia_relief_subbasins_big.png"},
+            {   label: "Blue Nile Tributaries relief and boundaries (76MB)",
+                src: "http://mint.isi.edu/dev/data/Blue_Nile_Tribs_relief_and_boundaries_big.png"},*/
+            {   label: "Guder relief rivers boundary",
+                src: "https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/ImproveQuality/Guder_relief_rivers_boundary.png"},
+            {   label: "Jamma relief river boundary",
+                src: "https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/ImproveQuality/Jamma_relief_river_boundary.png"},
+            {   label: "Muger relief rivers boundary",
+                src: "https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/ImproveQuality/Muger_relief_rivers_boundary.png"},
+            {   label: "Dashilo relief river boundary",
+                src: "https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/ImproveQuality/Dashilo_relief_river_boundary.png"},
+        ]
         return html`
-        <p>
-        This page is in progress, it will allow you to run tools to identify hydrological regions of interest. 
-        Below are some example hydrological regions identified for South Sudan:
-        <ul>
-        <li><a href="https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/PIHMRiverBasin/2017.png"
-            >South Sudan River Basins (PIHM)</a>. The three river basins that were our focus in 2018.</li>
-        <li><a href="https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/PIHMRiverBasin/POI.png"
-            >South Sudan River Basins - POI</a>. The three river basins that were our focus in 2018 (overlayed with points of interest).</li>
-        <li><a href="https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/ImproveQuality/Ethiopia_relief_boundary.png"
-            >Ethiopia relief boundary</a>. 
-            <a href="http://mint.isi.edu/dev/data/Ethiopia_relief_subbasins_big.png">Ethiopia relief subbasins (94MB)</a>
-        </li>
-        <li><a href="http://mint.isi.edu/dev/data/Blue_Nile_Tribs_relief_and_boundaries_big.png"
-            >Blue Nile Tributaries relief and boundaries (76MB)</a></li>
-        <li><a href="https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/ImproveQuality/Guder_relief_rivers_boundary.png"
-            >Guder relief rivers boundary</a></li>
-        <li><a href="https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/ImproveQuality/Jamma_relief_river_boundary.png"
-            >Jamma relief river boundary</a></li>
-        <li><a href="https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/ImproveQuality/Muger_relief_rivers_boundary.png"
-            >Muger relief rivers boundary</a></li>
-        <li><a href="https://raw.githubusercontent.com/mintproject/EthiopiaDemo/master/ImproveQuality/Dashilo_relief_river_boundary.png"
-            >Dashilo relief river boundary</a></li>
-        </ul>            
-        </p>
+        <div class="content">
+            <p>
+                This page is in progress, it will allow you to run tools to identify hydrological regions of interest. 
+                Below are some example hydrological regions identified for South Sudan:
+            </p>
+            <div style="width: 90%; margin: 0px auto;">
+                <image-gallery style="--width: 300px; --height: 160px;" .items="${items}"></image-gallery>
+            </div>
+        </div>
         `
     }
 }
