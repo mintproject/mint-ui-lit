3aa46d536456fcc46e5f094103177485b67846d8
Hernán Vargas <hern.var@gmail.com>
2020-06-02 03:20:53 -0400

    Added description to numerical indexes

--- src/screens/models/configure/resources/numerical-index.ts
+++ src/screens/models/configure/resources/numerical-index.ts
@@ -12,7 +12,6 @@ import { ExplorerStyles } from '../../model-explore/explorer-styles'
 
 import { Textfield } from 'weightless/textfield';
 import { Textarea } from 'weightless/textarea';
-import { Select } from 'weightless/select';
 
 @customElement('model-catalog-numerical-index')
 export class ModelCatalogNumericalIndex extends connect(store)(ModelCatalogResource)<NumericalIndex> {
@@ -29,26 +28,33 @@ export class ModelCatalogNumericalIndex extends connect(store)(ModelCatalogResou
         let edResource = this._getEditingResource();
         return html`
         <form>
-            <wl-textfield id="numericalIndex-label" label="Name" required
+            <wl-textfield id="index-label" label="Name" required
                 value=${edResource ? getLabel(edResource) : ''}>
             </wl-textfield>
+            <wl-textarea id="index-desc" label="Description" required
+                value=${edResource && edResource.description ? edResource.description[0] : ''}>
+            </wl-textarea>
         </form>`;
     }
 
     protected _getResourceFromForm () {
         // GET ELEMENTS
-        let inputLabel : Textfield = this.shadowRoot.getElementById('numericalIndex-label') as Textfield;
+        let inputLabel : Textfield = this.shadowRoot.getElementById('index-label') as Textfield;
+        let inputDesc : Textarea = this.shadowRoot.getElementById('index-desc') as Textarea;
         // VALIDATE
         let label : string = inputLabel ? inputLabel.value : '';
-        if (label) {
+        let desc : string = inputDesc ? inputDesc.value : '';
+        if (label && desc) {
             let jsonRes = {
                 type: ["NumericalIndex"],
                 label: [label],
+                description: [desc],
             };
             return NumericalIndexFromJSON(jsonRes);
         } else {
             // Show errors
             if (!label) (<any>inputLabel).onBlur();
+            if (!desc) (<any>inputDesc).onBlur();
         }
     }
 
