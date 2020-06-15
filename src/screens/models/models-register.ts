0144a487ece4c3f64f0787ed7231449dd9f7f65d
Hernán Vargas <hern.var@gmail.com>
2020-06-15 18:55:06 -0400

    Model register part 1

--- src/screens/models/models-register.ts
+++ src/screens/models/models-register.ts
@@ -4,19 +4,31 @@ import { PageViewElement } from '../../components/page-view-element';
 import { SharedStyles } from '../../styles/shared-styles';
 import { store, RootState } from 'app/store';
 import { connect } from 'pwa-helpers/connect-mixin';
+import { goToPage } from 'app/actions';
 
 import './configure/resources/person';
 import './configure/resources/grid';
 
 import { ModelCatalogPerson } from './configure/resources/person';
 import { ModelCatalogGrid } from './configure/resources/grid';
+import { ModelCatalogFundingInformation } from './configure/resources/funding-information';
+import { ModelCatalogVisualization } from './configure/resources/visualization';
 
+import { renderNotifications } from "util/ui_renders";
+import { showNotification } from 'util/ui_functions';
+
+import { modelPost } from 'model-catalog/actions';
+import { Model, ModelFromJSON } from '@mintproject/modelcatalog_client';
+import { getId } from 'model-catalog/util';
+
+import 'components/loading-dots'
 import "weightless/title";
 import "weightless/icon";
 import "weightless/select";
 import "weightless/textfield";
 
 import { Textfield } from 'weightless/textfield';
+import { Textarea } from 'weightless/textarea';
 import { Select } from 'weightless/select';
 
 @customElement('models-register')
@@ -146,10 +158,15 @@ export class ModelsRegister extends connect(store)(PageViewElement) {
     @property({type: Boolean})
     private _hideLateral : boolean = false;
 
+    @property({type: Boolean})
+    private _waiting : boolean = false;
+
     private _inputAuthor : ModelCatalogPerson;
     private _inputContributor : ModelCatalogPerson;
     private _inputContactPerson : ModelCatalogPerson;
     private _inputGrid : ModelCatalogGrid;
+    private _inputFunding : ModelCatalogFundingInformation;
+    private _inputVisualization : ModelCatalogVisualization;
 
     public constructor () {
         super();
@@ -157,10 +174,13 @@ export class ModelsRegister extends connect(store)(PageViewElement) {
         this._inputContributor = new ModelCatalogPerson();
         this._inputContactPerson = new ModelCatalogPerson();
         this._inputGrid = new ModelCatalogGrid();
+        this._inputFunding = new ModelCatalogFundingInformation();
+        this._inputVisualization = new ModelCatalogVisualization();
 
-        [this._inputAuthor, this._inputContributor, this._inputContactPerson]
+        [this._inputAuthor, this._inputContributor, this._inputContactPerson, this._inputVisualization]
                 .forEach((input) => input.setActionMultiselect());
-        this._inputGrid.setActionSelect();
+        [this._inputGrid, this._inputFunding]
+                .forEach((input) => input.setActionSelect());
     }
 
     protected render() {
@@ -181,11 +201,15 @@ export class ModelsRegister extends connect(store)(PageViewElement) {
                     </div>
                     ${this._renderStepForm()}
                     <div class="footer">
-                        <wl-button> continue </wl-button>
+                        <wl-button @click="${this._onContinueButtonClicked}" .disabled="${this._waiting}">
+                            continue 
+                            ${this._waiting ? html`<loading-dots style="--width: 20px; margin-left: 5px;"></loading-dots>` : ''}
+                        </wl-button>
                     </div>
                 </div>
             </div>
         </div>
+        ${renderNotifications()}
         `
     }
 
@@ -253,7 +277,7 @@ export class ModelsRegister extends connect(store)(PageViewElement) {
                 </tr>
 
                 <tr>
-                    <td>Sort description:</td>
+                    <td>Short description:</td>
                     <td>
                         <textarea id="m-short-desc" name="Short description" rows="3"></textarea>
                     </td>
@@ -263,13 +287,6 @@ export class ModelsRegister extends connect(store)(PageViewElement) {
                     <td>Full description:</td>
                     <td>
                         <textarea id="m-desc" name="Description" rows="5"></textarea>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>Sort description:</td>
-                    <td>
-                        <textarea id="m-short-desc" name="Short description" rows="3"></textarea>
                     </td>
                 </tr>
 
@@ -342,15 +359,98 @@ export class ModelsRegister extends connect(store)(PageViewElement) {
                         ${this._inputGrid}
                     </td>
                 </tr>
+
+                <tr>
+                    <td>Funding:</td>
+                    <td>
+                        ${this._inputFunding}
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>Visualizations:</td>
+                    <td>
+                        ${this._inputVisualization}
+                    </td>
+                </tr>
             </table>
         `;
+    }
+
+    private _getResourceFromForm () {
+        let inputLabel : Textfield = this.shadowRoot.getElementById('m-name') as Textfield;
+        let inputCategory : Select = this.shadowRoot.getElementById('m-category') as Select;
+        let inputShortDesc : Textarea = this.shadowRoot.getElementById("m-short-desc") as Textarea;
+        let inputDesc : Textarea = this.shadowRoot.getElementById("m-desc") as Textarea;
+        let inputExample : Textarea = this.shadowRoot.getElementById("m-example") as Textarea;
+        let inputKeywords : Textfield = this.shadowRoot.getElementById("m-keywords") as Textfield;
+        let inputLicense : Textarea = this.shadowRoot.getElementById("m-license") as Textarea;
+        let inputCitation : Textarea = this.shadowRoot.getElementById("m-citation") as Textarea;
+        let inputWebsite : Textfield = this.shadowRoot.getElementById("m-website") as Textfield;
+        let inputDocumentation : Textfield = this.shadowRoot.getElementById("m-documentation") as Textfield;
+
+        let label : string = inputLabel ? inputLabel.value : '';
+        let category  : string =       inputCategory?     inputCategory.value : '';
+        let shortDesc  : string =      inputShortDesc?    inputShortDesc.value : '';
+        let desc  : string =           inputDesc?         inputDesc.value : '';
+        let example  : string =        inputExample?      inputExample.value : '';
+        let keywords  : string =       inputKeywords?     inputKeywords.value : '';
+        let license  : string =        inputLicense?      inputLicense .value : '';
+        let citation  : string =       inputCitation?     inputCitation.value : '';
+        let website  : string =        inputWebsite?      inputWebsite.value : '';
+        let documentation  : string =  inputDocumentation? inputDocumentation.value : '';
+
+        if (label && desc && category) {
+            let jsonRes = {
+                type: ["Model"],
+                label: [label],
+                hasModelCategory: [category],
+                description: [desc],
+                contributor: this._inputContributor.getResources(),
+                hasContactPerson: this._inputContactPerson.getResources(),
+                author: this._inputAuthor.getResources(),
+                hasGrid: this._inputGrid.getResources(),
+                hasFunding: this._inputFunding.getResources(),
+                hasSampleVisualization: this._inputVisualization.getResources(),
+            };
+            if (shortDesc) jsonRes["shortDescription"] = [shortDesc];
+            if (example) jsonRes["hasExample"] = [example];
+            if (keywords) jsonRes["keywords"] = [keywords];
+            if (license) jsonRes["license"] = [license];
+            if (citation) jsonRes["citation"] = [citation];
+            if (website) jsonRes["website"] = [website];
+            if (documentation) jsonRes["hasDocumentation"] = [documentation];
+            //TODO:DATE
+
+            return ModelFromJSON(jsonRes);
+        } else {
+            if (!label) (<any>inputLabel).onBlur();
+            if (!category) (<any>inputCategory).onBlur();
+        }
+    }
+
+    private _onContinueButtonClicked () {
+        let newModel = this._getResourceFromForm();
+        if (newModel) {
+            showNotification("saveNotification", this.shadowRoot!);
+            this._waiting = true;
+            let req = store.dispatch(modelPost(newModel));
+            req.then((model:Model) => {
+                this._waiting = false;
+                console.log('new model!', model);
+                let url = 'models/explore/' + getId(model);
+                goToPage(url);
+            });
+        }
     }
 
     private _shouldClear : boolean = false;
     stateChanged(state: RootState) {
         if (state.app) {
             if (state.app.subpage === 'register') {
-                if (this._shouldClear) [this._inputAuthor, this._inputContributor, this._inputContactPerson, this._inputGrid]
+                if (this._shouldClear)
+                    [this._inputAuthor, this._inputContributor, this._inputContactPerson,
+                     this._inputGrid, this._inputFunding, this._inputVisualization]
                         .forEach((input) => input.setResources(null));
             } else {
                 this._shouldClear = true;
