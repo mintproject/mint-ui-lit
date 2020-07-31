bc8e3ec1a2a440d2544294968af28f3b7c805f7f
Varun Ratnakar <varunratnakar@gmail.com>
2020-07-31 19:59:07 +0530

    Fetch ensembleids if there are none yet

--- src/screens/modeling/pathway/mint-runs.ts
+++ src/screens/modeling/pathway/mint-runs.ts
@@ -316,7 +316,8 @@ export class MintRuns extends connect(store)(MintPathwayPage) {
                 hideNotification("runNotification", me.shadowRoot);
             },
             onError: function() {
-                console.log("Could not send");
+                hideNotification("runNotification", me.shadowRoot);
+                alert("Could not connect to the Ensemble Manager!");
             }
         }, data, false);
     }
@@ -376,7 +377,7 @@ export class MintRuns extends connect(store)(MintPathwayPage) {
     async _fetchRuns (modelid: string, currentPage: number, pageSize: number) {
         this.currentPage[modelid] = currentPage;
         
-        if(!this.pathwayModelEnsembleIds[modelid]) {
+        if(!this.pathwayModelEnsembleIds[modelid] || this.pathwayModelEnsembleIds[modelid].length == 0) {
             this.pathwayModelEnsembleIds[modelid] =  await getAllPathwayEnsembleIds(this.scenario.id, this.pathway.id, modelid);
         }
         
