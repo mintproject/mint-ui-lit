523c1369f6368af2b7921ad8360c4f56fb7a4ce7
Hernán Vargas <hern.var@gmail.com>
2020-08-27 00:00:32 -0400

    removing old code

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,15 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-//import dotenv from "dotenv";
-//import { FIREBASE_GOOGLE_API_KEY } from "./google-api-key";
 import { FIREBASE_PROD_CONF, FIREBASE_DEV_CONF } from "./google-api-key";
-
-/*if (process.env.NODE_ENV === "production") {
-    dotenv.config({ path: '../../.env' })
-} else {
-    dotenv.config({ path: '../../.env.dev' })
-};*/
 
 console.log("Initializing", process.env.NODE_ENV, "database");
 
@@ -20,27 +12,6 @@ if (process.env.NODE_ENV === "production") {
 } else {
     firebaseConfig = FIREBASE_DEV_CONF;
 }
-
-/*const firebaseConfig = {
-    apiKey: FIREBASE_GOOGLE_API_KEY,
-    authDomain: "mint-full.firebaseapp.com",
-    databaseURL: "https://mint-full.firebaseio.com",
-    projectId: "mint-full",
-    storageBucket: "mint-full.appspot.com",
-    messagingSenderId: "128464392367",
-    appId: "1:128464392367:web:97eaa44bc1fcb5296c445a"
-};
-
-/*const firebaseConfig = {
-    apiKey: process.env.FIREBASE_GOOGLE_API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId:  process.env.PROJECT_ID,
-    storageBucket:  process.env.STORAGE_BUCKET,
-    messagingSenderId:  process.env.MESSAGING_SENDER_ID,
-    appId:  process.env.APP_ID,
-};*/
-
 
 export const app = firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth(app);
