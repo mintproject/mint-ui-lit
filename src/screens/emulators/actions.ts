73b689f9ac416b5fb8ca37371cc7e83cda3e0885
Varun Ratnakar <varunratnakar@gmail.com>
2020-12-17 20:13:41 +0530

    Not querying fixed values for now, to make querying faster

--- src/screens/emulators/actions.ts
+++ src/screens/emulators/actions.ts
@@ -171,10 +171,10 @@ const getExecutionWhereClauseForFilter = (filters: EmulatorSearchConstraint[]) =
             " model_parameter: { name: { _eq: \"" + filter.input +"\"} } " +
             " parameter_value: {_in: [ " + filter.values.map((v) => "\"" + v + "\"") + " ]}\n" +
             "}}\n" +
-            "{ model: { parameters: {" + 
+            /*"{ model: { parameters: {" + 
             " name: { _eq: \"" + filter.input +"\"} " +
             " fixed_value: {_in: [ " + filter.values.map((v) => "\"" + v + "\"") + " ]}\n" +
-            "}}}\n" +
+            "}}}\n" +*/
             "]}\n";
     });
     query += filters.filter((filter) => filter.inputtype == "input").map((filter) => {
@@ -183,10 +183,10 @@ const getExecutionWhereClauseForFilter = (filters: EmulatorSearchConstraint[]) =
         " model_io: { name: { _eq: \"" + filter.input +"\"} }" +
         " resource_id: {_in: [ " + filter.values.map((v) => "\"" + v.id + "\"") + " ]}\n" +
         "}}\n" +
-        "{ model: { inputs: { model_io: {" + 
+        /*"{ model: { inputs: { model_io: {" + 
         " name: { _eq: \"" + filter.input +"\"} " +
         " fixed_bindings: { resource_id: {_in: [ " + filter.values.map((v) => "\"" + v.id + "\"") + " ]}}\n" +
-        "}}}}\n" +
+        "}}}}\n" +*/
         "]}\n";
     });
     query += " ]\n";
