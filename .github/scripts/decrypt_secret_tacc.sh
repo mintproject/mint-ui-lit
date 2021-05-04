8d9be90ee1f6f823c57edd36ccf3c12d260a0c38
Maximiliano Osorio <maxiosorio@gmail.com>
2021-05-04 14:26:25 -0400

    fix: decrypt

--- .github/scripts/decrypt_secret_tacc.sh
+++ .github/scripts/decrypt_secret_tacc.sh
@@ -2,6 +2,8 @@
 
 # Decrypt the file
 mkdir $HOME/secrets
-gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output $HOME/secrets/config.json config.json.gpg
-cp config.json.gpg src/config/
+gpg --quiet --batch --yes --decrypt \
+    --passphrase="$LARGE_SECRET_PASSPHRASE" \
+    --output $HOME/secrets/config.json \
+    config-tacc.json.gpg
+cp $HOME/secrets/config.json src/config/
