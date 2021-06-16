852988e07d54bb69d3eb573ce9d5036fe41d2438
Maximiliano Osorio <maxiosorio@gmail.com>
2021-06-16 11:07:30 -0400

    fix: remove hardcoded datacatalog

--- src/components/data-catalog-id-checker.ts
+++ src/components/data-catalog-id-checker.ts
@@ -2,6 +2,10 @@ import { customElement, LitElement, property, html, css } from "lit-element";
 
 import "weightless/button";
 import "weightless/icon";
+import * as mintConfig from 'config/config.json';
+import { MintPreferences, User } from 'app/reducers';
+
+let prefs = mintConfig["default"] as MintPreferences;
 
 @customElement('data-catalog-id-checker')
 export class DataCatalogIdChecker extends LitElement {
@@ -35,7 +39,7 @@ export class DataCatalogIdChecker extends LitElement {
     }
 
     private _load () {
-        let req = fetch("https://data-catalog.mint.isi.edu/datasets/get_dataset_info", {
+        let req = fetch(prefs.data_catalog_api + "/datasets/get_dataset_info", {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({"dataset_id": this.id})
