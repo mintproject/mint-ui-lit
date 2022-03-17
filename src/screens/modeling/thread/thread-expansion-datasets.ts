5758212472c136cde6fd871f62496d31feb3e3af
Hernán Vargas <hern.var@gmail.com>
2022-03-16 22:25:30 -0300

    Added visibility options

--- src/screens/modeling/thread/thread-expansion-datasets.ts
+++ src/screens/modeling/thread/thread-expansion-datasets.ts
@@ -31,10 +31,24 @@ export class ThreadExpansionDatasets extends ThreadExpansion {
     @property({type:Object}) datasetSelector : DatasetSelector;
     @property({type:Object}) selectedInput : ModelIO;
     @property({type:Object}) localRegions : RegionMap;
+    @property({type:Object}) modelVisible : IdMap<boolean> = {};
     private threadDatasets : Dataset[];
 
     static get styles() {
         return [SharedStyles, this.generalStyles, css`
+            .model_title {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 4px 10px;
+            }
+            .model_status {
+                display: flex;
+                align-items: center;
+            }
+            .model_status > * {
+                padding: 0px 5px;
+            }
             .tooltip {
                 cursor: help;
                 display: inline-block;
@@ -110,29 +124,139 @@ export class ThreadExpansionDatasets extends ThreadExpansion {
         }
         return html`
             <div style="border: 1px solid #EEE; margin: 10px 0px;">
-                <!-- Table with models and inputs -->
-                <table class="pure-table pure-table-striped">
-                    <thead>
-                        <tr>
-                            <th><b>Input name</b></th>
-                            <th><b>Selected dataset</b></th>
-                        </tr>
-                    </thead>
-
-                    ${Object.values(thread.models).map((m:LocalModel) => html`
-                        <tr><td colspan=2 style="font-size: 1.02em; font-weight: bold;">
-                            <span style="color: #aaa;">MODEL:</span>
-                            <a href="#">${m.name}</a>
-                        </td></tr>
-                        ${m.input_files && m.input_files.length > 0 ? m.input_files.map((io:ModelIO) => this.renderDatasetRow(m, io)) : html`
-                        <tr><td colspan=2 style="text-align:center;">
-                            This model does not require any input
-                        </td></tr>`}
-                    `)}
-                    <tbody>
-                    </tbody>
-                </table>
+                ${Object.values(thread.models).map(this.renderModelDatasets.bind(this))}
             </div>
+        `;
+    }
+
+    private toggleModelVisibility (modelid:string) : void {
+        this.modelVisible[modelid] = !this.modelVisible[modelid];
+        this.requestUpdate();
+    }
+
+    private renderModelDatasets (model:LocalModel) : TemplateResult {
+        let fixedInputs : ModelIO[] = model.input_files.filter((input:ModelIO) => input.value && input.value.resources);
+        let reqInputs : ModelIO[]   = model.input_files.filter((input:ModelIO) => !input.value || !input.value.resources || input.value.resources.length === 0);
+
+        // Check status
+        let modelBindings : ModelIOBindings = this.thread.model_ensembles![model.id].bindings || {};
+        let modelDone : boolean = fixedInputs.length > 0 && reqInputs.every((input:ModelIO) => 
+                (this.modifiedInputs[input.id] && this.modifiedInputs[input.id].length > 0) ||
+                ((modelBindings[input.id]||[]).map((id:string) => this.thread.data![id]).length > 0)
+        );
+
+        return html`
+            <div class="model_title">
+                <div class="model_status">
+                    ${modelDone ? html`
+                        <wl-icon style="color: 'green'">done</wl-icon>
+                    ` : html`
+                        <wl-icon style="color: 'orange'">warning</wl-icon>
+                    `}
+                    <span style="color: #aaa;">MODEL:</span>
+                    <a href="#">${model.name}</a><!-- FIXME -->
+                </div>
+                <wl-icon class="clickable" @click=${() => this.toggleModelVisibility(model.id)}>
+                    ${this.modelVisible[model.id] ? 'visibility' : 'visibility_off'}
+                </wl-icon>
+            </div>
+
+            <!-- Table with inputs -->
+            <table class="pure-table pure-table-striped" style="visibility: ${this.modelVisible[model.id] ? 'visible' : 'collapse'}">
+                <thead>
+                    <tr>
+                        <th><b>Input name</b></th>
+                        <th><b>Selected dataset</b></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${fixedInputs.length + reqInputs.length === 0 ? html`
+                    <tr>
+                        <td colspan=2 style="text-align:center;">
+                            This model does not require any input
+                        </td>
+                    </tr>` : html`
+                    ${reqInputs.map((io:ModelIO) => this.renderRequiredDatasetRow(model, io))}
+                    ${fixedInputs.length > 0 ? html`
+                    <tr>
+                        <td colspan=2 style="font-weight:bold; padding-left:35px;"> The following inputs were pre-selected and cannot be changed: </td>
+                    </tr>
+                    ${fixedInputs.map((io:ModelIO) => this.renderFixedDatasetRow(model, io))}
+                    ` : ""}
+                    `}
+                </tbody>
+            </table>`;
+    }
+
+    private renderFixedDatasetRow (model:LocalModel, input:ModelIO) : TemplateResult {
+        if (!input.value || !input.value.resources) return html``;
+        return html`
+            <tr>
+                <td> 
+                    <div style="display:flex; align-items:center;">
+                        <wl-icon style="color: 'green'; margin-right: 5px;">done</wl-icon>
+                        ${input.name} 
+                    </div>
+                </td>
+                <td>
+                    <div style="display:flex; align-items:center; justify-content: space-between;">
+                        <ul class="datasetlist">
+                            ${input.value.resources.map((r) => html`<li><a target="_blank" href="${r.url}">${r.name}</a></li>`)}
+                        </ul>
+                        <span tip="This input was pre-selected. Cannot be changed" class="tooltip"><wl-icon>lock</wl-icon></span>
+                    </div>
+                </td>
+            </tr>
+        `;
+    }
+
+    private renderRequiredDatasetRow (model:LocalModel, input:ModelIO) : TemplateResult {
+        let modelBindings : ModelIOBindings = this.thread.model_ensembles![model.id].bindings || {};
+        let dataBindingIds : string[] = modelBindings[input.id] ? modelBindings[input.id] : [];
+        let dataslices : Dataslice[] = this.modifiedInputs[input.id] ?
+                this.modifiedInputs[input.id]
+                : dataBindingIds.map((bid:string) => this.thread.data![bid]);
+
+        return html`
+            <tr>
+                <td> 
+                    <div style="display:flex; align-items:center;">
+                        ${dataslices.length > 0 ? html`
+                            <wl-icon style="color: 'green'; margin-right: 5px;">done</wl-icon>
+                        ` : html`
+                            <wl-icon style="color: 'orange'; margin-right: 5px;">warning</wl-icon>
+                        `}
+                        ${input.name} 
+                    </div>
+                </td>
+                <td>
+                    <div style="display:flex; align-items:center; justify-content: space-between;">
+                        <ul class="datasetlist">
+                            ${dataslices.length == 0 ? "None selected" : dataslices.map((dataslice:Dataslice) => {
+                                if (!dataslice) return;
+                                let num_selected_resources = dataslice.selected_resources ?? 0;
+                                let num_total_resources = dataslice.total_resources ?? 0;
+                                return html`<li>
+                                <a target="_blank" href="${this._regionid}/datasets/browse/${dataslice.dataset.id}/${this.thread.regionid}">${dataslice.dataset.name}</a>
+                                ${num_total_resources > 1 ?
+                                    html`
+                                        <br />
+                                        ( ${num_selected_resources} / ${num_total_resources} files 
+                                        ${this.editMode && this.permission.write ? html `
+                                        - 
+                                        <a style="cursor:pointer">Change</a>` : ""} )
+                                    `
+                                    : ""}
+                                </li>`;
+                            })}
+                        </ul>
+
+                        ${this.editMode ? html`
+                            <wl-icon class="clickable" @click=${() => this.editInput(model, input)}>edit</wl-icon>`
+                        : html`<wl-icon>person</wl-icon>`}
+                    </div>
+                </td>
+            </tr>
         `;
     }
 
@@ -193,10 +317,6 @@ export class ThreadExpansionDatasets extends ThreadExpansion {
                 </td>
             </tr>
         `;
-    }
-
-    private renderDataslice (datasliceid:string) : TemplateResult {
-        return html``
     }
 
     private renderDatasetSelectionDialog () : TemplateResult {
@@ -282,7 +402,10 @@ export class ThreadExpansionDatasets extends ThreadExpansion {
     }
 
     protected onThreadChange(thread: Thread): void {
-        if (this.localRegions) this.getThreadDatasets();
+        if (this.localRegions)
+            this.getThreadDatasets();
+        if (this.thread.models)
+            Object.values(this.thread.models).forEach((m:LocalModel) => this.modelVisible[m.id] = true)
     }
 
     stateChanged(state: RootState) {
@@ -328,8 +451,20 @@ export class ThreadExpansionDatasets extends ThreadExpansion {
 
         this.datasetSelector.unload();
         this.loading = true;
-        let req : Promise<Dataset[]> = DataCatalogAdapter.findDatasetByVariableName(input.variables, region, dates)
+        let req : Promise<Dataset[]> = DataCatalogAdapter.findDatasetByVariableName(input.variables, region, dates);
         req.then((datasets:Dataset[]) => {
+            // Sort results depending of type.
+            if (input.type) {
+                let inputtype = input.type.replace(/.*#/, '');
+                datasets = datasets.sort((d1:Dataset,d2:Dataset) => {
+                    return d1.datatype === d2.datatype ? 0
+                        : ( d1.datatype === inputtype ? -1 
+                            : (d2.datatype === inputtype ? 1 
+                                : (!!d1.datatype && !d2.datatype ? -1
+                                    : (!!d2.datatype && !d1.datatype ? 1 : 0)
+                            )));
+                });
+            }
             this.datasetSelector.setDatasets(datasets, region, dates);
             this.loading = false;
         })
