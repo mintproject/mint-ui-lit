04344122abd09c7c40292391b9e4da8dbf7ce6bb
Maximilinano Osorio <mosorio@isi.edu>
2022-09-07 10:14:08 -0400

    add: configuration files

---
+++ config-dev.js
@@ -0,0 +1,38 @@
+window.REACT_APP_WELCOME_MESSAGE = "Welcome to MINT";
+
+// Mint Services
+window.REACT_APP_DATA_CATALOG_API = "https://data-catalog.mint.isi.edu";
+window.REACT_APP_CROMO_API = "https://cromo.wildfire.mint.isi.edu/v1.0";
+window.REACT_APP_MODEL_CATALOG_API = "https://api.models.dev.mint.isi.edu/v1.8.0";
+window.REACT_APP_ENSEMBLE_MANAGER_API = "https://dev.ensemble.mint.isi.edu/v1";
+
+// Google API Key
+window.REACT_APP_GOOGLE_MAPS_KEY = "AIzaSyBl8J85KibSuhHEw_j0JTlITVTBb8oXiMo";
+
+//Authentications
+window.REACT_APP_AUTH_SERVER = "https://auth.mint.isi.edu/auth/";
+window.REACT_APP_AUTH_REALM = "production";
+window.REACT_APP_AUTH_CLIENT_ID = "mint-ui";
+
+//Execution - Local
+window.REACT_APP_EXECUTION_ENGINE = "localex";
+window.REACT_APP_LOCALEX_CODEDIR = "/data/storage/mint/localex/code";
+window.REACT_APP_LOCALEX_DATADIR = "/data/storage/mint/data-catalog/production/local-execution";
+window.REACT_APP_LOCALEX_DATAURL = "https://data.mint.isi.edu/files/local-execution";
+window.REACT_APP_LOCALEX_LOGDIR = "/data/storage/mint/data-catalog/production/local-execution/logs";
+window.REACT_APP_LOCALEX_LOGURL = "https://data.mint.isi.edu/files/local-execution/logs";
+window.REACT_APP_LOCALEX_PARALLEL = 50;
+
+//Execution - Wings
+window.REACT_APP_WINGS_EXPORT_URL = "https://data.mint.isi.edu/files/wings-export";
+window.REACT_APP_WINGS_STORAGE = "/data/storage/mint/data-catalog/production/wings-export";
+window.REACT_APP_WINGS_DOTPATH = "/data/storage/mint/data-catalog/production/wings-export/dot";
+window.REACT_APP_WINGS_ONTURL = "https://data.mint.isi.edu/files/wings-export/dot";
+
+//Database
+window.REACT_APP_GRAPHQL_ENDPOINT = "graphql.dev.mint.isi.edu/v1/graphql";
+window.REACT_APP_GRAPHQL_ENABLE_SSL = "True";
+
+//Visualization and Ingestion
+window.REACT_APP_VISUALIZATION_URL = "https://dev.viz.mint.isi.edu";
+window.REACT_APP_INGESTION_API = "https://node1.ingestion.mint.isi.edu/v1.3.0";
